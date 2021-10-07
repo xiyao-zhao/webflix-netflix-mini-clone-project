e9977ab1a73be5b94945efba3a1281d77f531184
Xiyao Zhao <xiyao.zhao28@gmail.com>
2021-10-07 11:22:57 -0400

    Edit ItemList's arguments

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,13 +1,14 @@
 import classes from './ItemList.module.css';
 
-const ItemList = ({ items, Poster }) => {
+const ItemList = ({ items }) => {
+    
     return (
         <div className={classes.grid}>
             <ul>
                 {/* Map through the results array and display and content */}
-                {items.map((item) => (
-                    <li key={item.Poster} >
-                        <img src={item.Poster} alt={Poster} />
+                {items.map((item, index) => (
+                    <li key={item.imdbID + index} >
+                        <img src={item.Poster} alt={item.Title} />
                         <p>{ item.Title }</p>
                         <p>{ item.Year }</p>
                     </li>       
